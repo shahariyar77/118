1f83d89de03580c3ef51cf3811add3ea7ef74814
shahariyar Nafis <72545687+shahariyar77@users.noreply.github.com>
2020-11-30 20:12:54 +0600

    Add files via upload

---
+++ 118-carousel-item/js/main.js
@@ -0,0 +1,10 @@
+ $(document).ready(function(){
+     $(".potfolio-tiem").owlCarousel({
+         items:5,
+         loop:true,
+         margin:30,
+         autoplay:true,
+         dots:true
+         
+     })
+ });
